c073c722f46cecf5e8b612b546f61cea51d527c6
Fazrin <mfmfazrin1986@gmail.com>
2021-08-02 22:14:47 +0530

    basic calculator sample code

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace learningString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.ReadLine();
+        }
+    }
+
+    //write a class to reverse a atring
+    public class reverseString
+    {
+        public string reverse(string str)
+        {
+            string result = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                result += str[i];
+            }
+            return result;
+        }
+    }
+
+}
